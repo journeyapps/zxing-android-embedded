a01a3495e3897c4c5f5dab156bab652575b51de9
Hans Reichenbach <hans.reichenbach@okta.com>
2014-09-25 17:31:29 -0700

    update gitignore and changing tabbing in the classes I'm fiddling with

--- zxing-android/src/com/google/zxing/client/android/CaptureActivityHandler.java
+++ zxing-android/src/com/google/zxing/client/android/CaptureActivityHandler.java
@@ -45,119 +45,119 @@ import java.util.Map;
  */
 public final class CaptureActivityHandler extends Handler {
 
-  private static final String TAG = CaptureActivityHandler.class.getSimpleName();
+    private static final String TAG = CaptureActivityHandler.class.getSimpleName();
 
-  private final CaptureActivity activity;
-  private final DecodeThread decodeThread;
-  private State state;
-  private final CameraManager cameraManager;
+    private final CaptureActivity activity;
+    private final DecodeThread decodeThread;
+    private State state;
+    private final CameraManager cameraManager;
 
-  private enum State {
-    PREVIEW,
-    SUCCESS,
-    DONE
-  }
+    private enum State {
+        PREVIEW,
+        SUCCESS,
+        DONE
+    }
 
-  CaptureActivityHandler(CaptureActivity activity,
-                         Collection<BarcodeFormat> decodeFormats,
-                         Map<DecodeHintType,?> baseHints,
-                         String characterSet,
-                         CameraManager cameraManager) {
-    this.activity = activity;
-    decodeThread = new DecodeThread(activity, decodeFormats, baseHints, characterSet,
-        new ViewfinderResultPointCallback(activity.getViewfinderView()));
-    decodeThread.start();
-    state = State.SUCCESS;
+    CaptureActivityHandler(CaptureActivity activity,
+                           Collection<BarcodeFormat> decodeFormats,
+                           Map<DecodeHintType,?> baseHints,
+                           String characterSet,
+                           CameraManager cameraManager) {
+        this.activity = activity;
+        decodeThread = new DecodeThread(activity, decodeFormats, baseHints, characterSet,
+                new ViewfinderResultPointCallback(activity.getViewfinderView()));
+        decodeThread.start();
+        state = State.SUCCESS;
 
-    // Start ourselves capturing previews and decoding.
-    this.cameraManager = cameraManager;
-    cameraManager.startPreview();
-    restartPreviewAndDecode();
-  }
+        // Start ourselves capturing previews and decoding.
+        this.cameraManager = cameraManager;
+        cameraManager.startPreview();
+        restartPreviewAndDecode();
+    }
 
-  @Override
-  public void handleMessage(Message message) {
-    if (message.what == R.id.zxing_restart_preview) {
-      restartPreviewAndDecode();
+    @Override
+    public void handleMessage(Message message) {
+        if (message.what == R.id.zxing_restart_preview) {
+            restartPreviewAndDecode();
 
-    } else if (message.what == R.id.zxing_decode_succeeded) {
-      state = State.SUCCESS;
-      Bundle bundle = message.getData();
-      Bitmap barcode = null;
-      float scaleFactor = 1.0f;
-      if (bundle != null) {
-        byte[] compressedBitmap = bundle.getByteArray(DecodeThread.BARCODE_BITMAP);
-        if (compressedBitmap != null) {
-          barcode = BitmapFactory.decodeByteArray(compressedBitmap, 0, compressedBitmap.length, null);
-          // Mutable copy:
-          barcode = barcode.copy(Bitmap.Config.ARGB_8888, true);
+        } else if (message.what == R.id.zxing_decode_succeeded) {
+            state = State.SUCCESS;
+            Bundle bundle = message.getData();
+            Bitmap barcode = null;
+            float scaleFactor = 1.0f;
+            if (bundle != null) {
+                byte[] compressedBitmap = bundle.getByteArray(DecodeThread.BARCODE_BITMAP);
+                if (compressedBitmap != null) {
+                    barcode = BitmapFactory.decodeByteArray(compressedBitmap, 0, compressedBitmap.length, null);
+                    // Mutable copy:
+                    barcode = barcode.copy(Bitmap.Config.ARGB_8888, true);
+                }
+                scaleFactor = bundle.getFloat(DecodeThread.BARCODE_SCALED_FACTOR);
+            }
+            activity.handleDecode((Result) message.obj, barcode, scaleFactor);
+
+        } else if (message.what == R.id.zxing_decode_failed) {// We're decoding as fast as possible, so when one decode fails, start another.
+            state = State.PREVIEW;
+            cameraManager.requestPreviewFrame(decodeThread.getHandler(), R.id.zxing_decode);
+
+        } else if (message.what == R.id.zxing_return_scan_result) {
+            activity.setResult(Activity.RESULT_OK, (Intent) message.obj);
+            activity.finish();
+
+        } else if (message.what == R.id.zxing_launch_product_query) {
+            String url = (String) message.obj;
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            intent.setData(Uri.parse(url));
+
+            ResolveInfo resolveInfo =
+                    activity.getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            String browserPackageName = null;
+            if (resolveInfo != null && resolveInfo.activityInfo != null) {
+                browserPackageName = resolveInfo.activityInfo.packageName;
+                Log.d(TAG, "Using browser in package " + browserPackageName);
+            }
+
+            // Needed for default Android browser / Chrome only apparently
+            if ("com.android.browser".equals(browserPackageName) || "com.android.chrome".equals(browserPackageName)) {
+                intent.setPackage(browserPackageName);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra(Browser.EXTRA_APPLICATION_ID, browserPackageName);
+            }
+
+            try {
+                activity.startActivity(intent);
+            } catch (ActivityNotFoundException ignored) {
+                Log.w(TAG, "Can't find anything to handle VIEW of URI " + url);
+            }
+
         }
-        scaleFactor = bundle.getFloat(DecodeThread.BARCODE_SCALED_FACTOR);
-      }
-      activity.handleDecode((Result) message.obj, barcode, scaleFactor);
-
-    } else if (message.what == R.id.zxing_decode_failed) {// We're decoding as fast as possible, so when one decode fails, start another.
-      state = State.PREVIEW;
-      cameraManager.requestPreviewFrame(decodeThread.getHandler(), R.id.zxing_decode);
-
-    } else if (message.what == R.id.zxing_return_scan_result) {
-      activity.setResult(Activity.RESULT_OK, (Intent) message.obj);
-      activity.finish();
-
-    } else if (message.what == R.id.zxing_launch_product_query) {
-      String url = (String) message.obj;
-
-      Intent intent = new Intent(Intent.ACTION_VIEW);
-      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-      intent.setData(Uri.parse(url));
-
-      ResolveInfo resolveInfo =
-              activity.getPackageManager().resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
-      String browserPackageName = null;
-      if (resolveInfo != null && resolveInfo.activityInfo != null) {
-        browserPackageName = resolveInfo.activityInfo.packageName;
-        Log.d(TAG, "Using browser in package " + browserPackageName);
-      }
-
-      // Needed for default Android browser / Chrome only apparently
-      if ("com.android.browser".equals(browserPackageName) || "com.android.chrome".equals(browserPackageName)) {
-        intent.setPackage(browserPackageName);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra(Browser.EXTRA_APPLICATION_ID, browserPackageName);
-      }
-
-      try {
-        activity.startActivity(intent);
-      } catch (ActivityNotFoundException ignored) {
-        Log.w(TAG, "Can't find anything to handle VIEW of URI " + url);
-      }
-
-    }
-  }
-
-  public void quitSynchronously() {
-    state = State.DONE;
-    cameraManager.stopPreview();
-    Message quit = Message.obtain(decodeThread.getHandler(), R.id.zxing_quit);
-    quit.sendToTarget();
-    try {
-      // Wait at most half a second; should be enough time, and onPause() will timeout quickly
-      decodeThread.join(500L);
-    } catch (InterruptedException e) {
-      // continue
     }
 
-    // Be absolutely sure we don't send any queued up messages
-    removeMessages(R.id.zxing_decode_succeeded);
-    removeMessages(R.id.zxing_decode_failed);
-  }
+    public void quitSynchronously() {
+        state = State.DONE;
+        cameraManager.stopPreview();
+        Message quit = Message.obtain(decodeThread.getHandler(), R.id.zxing_quit);
+        quit.sendToTarget();
+        try {
+            // Wait at most half a second; should be enough time, and onPause() will timeout quickly
+            decodeThread.join(500L);
+        } catch (InterruptedException e) {
+            // continue
+        }
 
-  private void restartPreviewAndDecode() {
-    if (state == State.SUCCESS) {
-      state = State.PREVIEW;
-      cameraManager.requestPreviewFrame(decodeThread.getHandler(), R.id.zxing_decode);
-      activity.drawViewfinder();
+        // Be absolutely sure we don't send any queued up messages
+        removeMessages(R.id.zxing_decode_succeeded);
+        removeMessages(R.id.zxing_decode_failed);
     }
-  }
+
+    private void restartPreviewAndDecode() {
+        if (state == State.SUCCESS) {
+            state = State.PREVIEW;
+            cameraManager.requestPreviewFrame(decodeThread.getHandler(), R.id.zxing_decode);
+            activity.drawViewfinder();
+        }
+    }
 
 }
